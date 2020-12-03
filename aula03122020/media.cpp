83648ab4124a3e9c886af6f19d148949ce7daded
silviocs <silviocs@imd.ufrn.br>
2020-12-03 19:44:11 -0300

    Exemplo da aula de arquivos.

---
+++ aula03122020/media.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using std::stringstream;
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::endl;
+using std::ios;
+using std::vector;
+using std::fixed;
+using std::setprecision;
+using std::ostream;
+using std::istream;
+
+class Aluno
+{
+public:
+	Aluno(string nome, double nota1, double nota2, double nota3):
+		nome(nome), nota1(nota1), nota2(nota2), nota3(nota3) {}
+	Aluno() {}
+	~Aluno() {}
+	string getNome() const {
+		return this->nome;
+	}
+	double getNota1() const {
+		return this->nota1;
+	}
+	double getNota2() const {
+		return this->nota2;
+	}
+	double getNota3() const {
+		return this->nota3;
+	}
+	double getMedia() const {
+		return (this->nota1+this->nota2+this->nota3)/3;
+	}
+	string getStatus() const {
+		double media = this->getMedia();
+		if (media >= 7.0) {
+			return "Aprovado";
+		}
+		return "Reprovado";
+	}
+	friend ostream& operator<< (ostream &o, Aluno const aluno) {
+		o << aluno.nome << " " << fixed << setprecision(1) << aluno.getMedia() << " " << aluno.getStatus();
+		return o;
+	}
+	friend istream& operator>> (istream &i, Aluno aluno) {
+		i >> aluno.nome >> aluno.nota1 >> aluno.nota2 >> aluno.nota3;
+		return i;
+	}
+private:
+	string nome;
+	double nota1;
+	double nota2;
+	double nota3;
+};
+
+int main(int argc, char const *argv[])
+{
+	ifstream arqDados("media.dat");
+	string linha;
+	string palavra;
+
+	while(arqDados >> linha) {
+		// quebrando a linha 
+        stringstream s(linha); 
+        vector <string> tokens; 
+        while (getline(s, palavra, ';')) {
+        	tokens.push_back(palavra); 
+        }
+        if (tokens.size() == 4) {
+        	Aluno lido(tokens.at(0), stod(tokens.at(1)), stod(tokens.at(2)), stod(tokens.at(3)));
+        	cout << lido << endl;
+        }
+	}
+	return 0;
+}
